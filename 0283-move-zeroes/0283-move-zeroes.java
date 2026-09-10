a59cf770927ed3a3435749a7b23c7762abbe0c07
Tanuuuu <223143450+tanupatwari05@users.noreply.github.com>
2026-09-10 23:39:12 +0530

    Time: 2 ms (91.79%), Space: 47.8 MB (57.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,22 +1,62 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0;
 
-        // Put all non-zero elements at the front
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[j] = nums[i];
-                j++;
+        // optimal solution 
+
+        int n = nums.length;
+        // step-1  find the zeorth element 
+        int j = -1; 
+        for(int i = 0;i<n;i++){
+            if(nums[i]== 0){
+                j=i;
+                break;
             }
-        }
 
-        // Fill the remaining positions with zeroes
-        while (j < nums.length) {
-            nums[j] = 0;
+        }
+        // if there is no zero array is laready correct 
+         if( j == -1) {
+            return;
+         } 
+         // for non zero no in array (shifting them in before)
+        for(int i = j+1;i<n;i++){
+            if(nums[i]!= 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp ;
+            
+            
             j++;
+            }
         }
     }
 }
+//         // brute force approach 
+//         // step 1  taking out non zero nums from the array 
+    
+//         int n = nums.length;
+//         int [] temp = new int [n] ;
+//          int index = 0;
+
+//         for(int i = 0; i<n;i++){
+//             if(nums[i]!=0){
+//                 temp[index]= nums[i];
+//                 index++;
+//             }
+//         }
+//         // step 2  add temp arr into original nums 
+
+//          for(int i = 0 ;i< index;i++){
+//             nums[i] = temp[i];
+//         }
+
+//         // step 3  add zeroes  at the end of the arrray 
+
+//         for(int i = index ;i<n;i++){
+//             nums[i] = 0;
+//         }
+//     }
+// }
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
